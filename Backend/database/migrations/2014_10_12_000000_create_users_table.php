f7ee97b9b81d2e27452755e7a9a572fdc0c202a1
Mona-Ali-Mostafa98 <monaali980628@gmail.com>
2024-06-12 10:55:44 +0300

    rearange files

--- Backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ Backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone_number')->unique();
             $table->string('password');
+            $table->string('image');
+            $table->enum('gender', ['female', 'male'])->nullable();
+            $table->enum('role', ['user', 'business'])->default('user');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
