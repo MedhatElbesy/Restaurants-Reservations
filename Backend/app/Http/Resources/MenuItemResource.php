18c6b446b7cff670d1e4403f5cfbbc8504489ece
Blitz576 <anagy8050@gmail.com>
2024-06-23 21:25:53 +0300

    refactor(api) : Provided resturant menu details with resturant by id request

---
+++ Backend/app/Http/Resources/MenuItemResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MenuItemResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'menu_category_id' => $this->menu_category_id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'description' => $this->description,
+            'price' => $this->price,
+            'sale_price' => $this->sale_price,
+            'status' => $this->status,
+        ];
+    }
+}
