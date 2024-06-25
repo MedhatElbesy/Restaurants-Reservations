ae1533777f8fefe29db43352e33cca421718eed0
Medhat Elbesy <152287116+MedhatElbesy@users.noreply.github.com>
2024-06-25 16:31:48 +0300

    Add seeder

--- Backend/database/seeders/RestaurantCategorySeeder.php
+++ Backend/database/seeders/RestaurantCategorySeeder.php
@@ -29,6 +29,54 @@ class RestaurantCategorySeeder extends Seeder
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
+            [
+                'restaurant_id' => 3,
+                'category_id' => 1,
+                'status' => ItemStatus::Enabled,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'restaurant_id' => 4,
+                'category_id' => 2,
+                'status' => ItemStatus::Enabled,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'restaurant_id' => 5,
+                'category_id' => 3,
+                'status' => ItemStatus::Enabled,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'restaurant_id' => 7,
+                'category_id' => 3,
+                'status' => ItemStatus::Enabled,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'restaurant_id' => 8,
+                'category_id' => 4,
+                'status' => ItemStatus::Enabled,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'restaurant_id' => 9,
+                'category_id' => 4,
+                'status' => ItemStatus::Enabled,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],[
+                'restaurant_id' => 10,
+                'category_id' => 4,
+                'status' => ItemStatus::Enabled,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
         ]);
     }
 }
