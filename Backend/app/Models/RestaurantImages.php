6840fe9dec5dc54758699526c1fcd46ae41c413f
Blitz576 <anagy8050@gmail.com>
2024-07-12 16:29:10 +0300

    refactor : get all the images urls

--- Backend/app/Models/RestaurantImages.php
+++ Backend/app/Models/RestaurantImages.php
@@ -5,11 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
 
 class RestaurantImages extends Model
 {
     use HasFactory;
     public $guarded = ['id','created_at','updated_at'];
+
+    public function getImageUrlAttribute()
+    {
+        if (!$this->image) {
+            return 'https://static.vecteezy.com/system/resources/thumbnails/023/523/095/small_2x/3d-rendering-of-a-restaurant-building-illustration-png.png';
+        }
+        if (Str::startsWith($this->image, ['http://', 'https://'])) {
+            return $this->image;
+        }
+        return url('storage/' . $this->image);
+    }
    public function restaurant() : BelongsTo{
         return $this->belongsTo(Restaurant::class);
     }
