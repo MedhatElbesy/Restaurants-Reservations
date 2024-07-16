ae21a53047976a8cc6e042cfe299cf1b68e60e10
ayaadel1346 <156436119+ayaadel1346@users.noreply.github.com>
2024-07-16 23:41:45 +0300

    changes at footer

--- react/src/layouts/footer/Footer.jsx
+++ react/src/layouts/footer/Footer.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF, faTwitter, faGoogle, faInstagram, faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
 import './Footer.css';
+import { faEnvelope, faInfoCircle, faShieldAlt } from '@fortawesome/free-solid-svg-icons';
 
 
 export default function Footer() {
   return (
     <>
-      <footer className="footer text-center text-white">
+      <footer className=" myfooter text-center text-white">
         <div className="container py-4 footer-data">
           <section className="mb-4">
             <a className="btn btn-outline-light m-1" href="#!" role="button">
@@ -20,53 +21,53 @@ export default function Footer() {
               <FontAwesomeIcon icon={faGoogle} />
             </a>
             <a className="btn btn-outline-light m-1" href="#!" role="button">
-              <FontAwesomeIcon icon={faInstagram} />
+              <FontAwesomeIcon icon={faInfoCircle} />
             </a>
             <a className="btn btn-outline-light m-1" href="#!" role="button">
-              <FontAwesomeIcon icon={faLinkedinIn} />
+              <FontAwesomeIcon icon={faEnvelope} />
             </a>
             <a className="btn btn-outline-light m-1" href="#!" role="button">
-              <FontAwesomeIcon icon={faGithub} />
+              <FontAwesomeIcon icon={faShieldAlt} />
             </a>
           </section>
 
           <section className="row">
             <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-              <h5 className="text-uppercase">About Us</h5>
+           
               <ul className="list-unstyled mb-0">
-                <li><a href="#!" className="text-white text-decoration-none">Home</a></li>
-                <li><a href="#!" className="text-white text-decoration-none">Contact</a></li>
-                <li><a href="#!" className="text-white text-decoration-none">Email</a></li>
+                
+                <li><a href="#!" className="text-dark text-decoration-none">Email</a></li>
+                <li><a href="#!" className="text-dark text-decoration-none">Restau@gmail.com</a></li>
               </ul>
             </div>
             <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-              <h5 className="text-uppercase">Services</h5>
+         
               <ul className="list-unstyled mb-0 ">
-                <li><a href="#!" className="text-white text-decoration-none">Service 1</a></li>
-                <li><a href="#!" className="text-white text-decoration-none">Service 2</a></li>
-                <li><a href="#!" className="text-white text-decoration-none">Service 3</a></li>
+                
+                <li><a href="#!" className="text-dark text-decoration-none">Contact Us</a></li>
+                <li><a href="#!" className="text-dark text-decoration-none">+44 20 7946 0958</a></li>
               </ul>
             </div>
             <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-              <h5 className="text-uppercase">Support</h5>
+           
               <ul className="list-unstyled mb-0">
-                <li><a href="#!" className="text-white text-decoration-none">Contact Us</a></li>
-                <li><a href="#!" className="text-white text-decoration-none">Help Center</a></li>
-                <li><a href="#!" className="text-white text-decoration-none">Support</a></li>
+                
+                <li><a href="#!" className="text-dark text-decoration-none">Help Center</a></li>
+                <li><a href="#!" className="text-dark text-decoration-none">+44 20 7946 0958</a></li>
               </ul>
             </div>
             <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-              <h5 className="text-uppercase">Follow Us</h5>
+            
               <ul className="list-unstyled mb-0">
-                <li><a href="#!" className="text-white text-decoration-none">Facebook</a></li>
-                <li><a href="#!" className="text-white text-decoration-none">Twitter</a></li>
-                <li><a href="#!" className="text-white text-decoration-none">Instagram</a></li>
+                <li><a href="#!" className="text-dark text-decoration-none">Follow Us</a></li>
+                <li><a href="#!" className="text-dark text-decoration-none">Facebook</a></li>
+                
               </ul>
             </div>
           </section>
         </div>
 
-        <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        <div className="text-center p-3 copyright">
             <p>
           © 2020 Copyright:Restau
           </p>
