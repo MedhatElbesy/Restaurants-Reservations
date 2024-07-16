75656097d35d50addd9fc338f142edf6dbaff35a
ayaadel1346 <156436119+ayaadel1346@users.noreply.github.com>
2024-07-17 00:11:25 +0300

    edit card

--- react/src/components/homepage/card/Card.jsx
+++ react/src/components/homepage/card/Card.jsx
@@ -1,9 +1,9 @@
 import React, { useContext } from 'react';
 import './Card.css';
-import { BodyColorContext } from '../../../BodyColorContext';
+
 
 export default function Card({ name, image }) {
-  const { bodyColor } = useContext(BodyColorContext);
+ 
 
   return (
     <div
@@ -11,9 +11,9 @@ export default function Card({ name, image }) {
 
     >
       <main className="row">
-        <div className="restaurant-card my-5 col-md-0 col-12">
-          <img style={{ height: '40vh', width: '20vw' }} src={image} alt={name} />
-          <h3 style={{ color: bodyColor === 'light' ? '#333' : '#ca8e46' }}>{name}</h3>
+        <div className="restaurant-card my-5 col-md-12 col-12">
+          <img src={image} alt={name} />
+          <h4 className='text-decoration-none'>{name}</h4>
         </div>
       </main>
     </div>
