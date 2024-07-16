a632fc08a0b7d6a66edf9378c41e8c1519d2369d
ayaadel1346 <156436119+ayaadel1346@users.noreply.github.com>
2024-07-17 00:03:35 +0300

    add all restaurants page

--- react/src/components/homepage/CustomCarousel.jsx
+++ react/src/components/homepage/CustomCarousel.jsx
@@ -1,43 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./CustomCarousel.css";
 
 const CustomCarousel = () => {
-  const [titleIndex, setTitleIndex] = useState(0);
-  const titles = [
-    "Taste the Diversity of Cuisine",
-    "Discover a World of Flavor",
-    "Every Meal Tells a Story",
-  ];
-
-  useEffect(() => {
-    const titleInterval = setInterval(() => {
-      setTitleIndex((prevIndex) => (prevIndex + 1) % titles.length);
-    }, 8050);
-
-    return () => {
-      clearInterval(titleInterval);
-    };
-  }, [titles.length]);
-
   return (
     <main className="video-container container-fluid px-0">
+
       <div className="overlay"></div>
 
-      <section className="row">
-        <video className="col-12" autoPlay loop muted>
-          <source
-            src="https://matchthemes.com/demowp/caverta/wp-content/uploads/video-home.mp4"
-            type="video/mp4"
-          />
-          Your browser does not support the video tag.
-        </video>
-      </section>
+      <video className="video-element" autoPlay loop muted>
+        <source
+          src="./images/header_video_ January_2022.mp4"
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video>
 
-      <section className="row">
-        <header className="col-12">
-          <h1 className="my-5 display-5">{titles[titleIndex]}</h1>
-        </header>
+      <section className="text-container col-6">
+
+        <h1 className="main-text">
+          Delicious <strong>Moments</strong>
+        </h1>
+
+        <h1 className="sub-text">
+          at The Gourmet Experience
+        </h1>
+
+        <p className="caption">Where every meal is a celebration</p>
+        
       </section>
+      
     </main>
   );
 };
