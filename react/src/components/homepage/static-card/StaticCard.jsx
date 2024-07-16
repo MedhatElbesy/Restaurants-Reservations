5c237361cd29c6b92c7b9c058572cb5e103e668a
ayaadel1346 <156436119+ayaadel1346@users.noreply.github.com>
2024-07-17 00:06:15 +0300

    edit static cards

--- react/src/components/homepage/static-card/StaticCard.jsx
+++ react/src/components/homepage/static-card/StaticCard.jsx
@@ -28,20 +28,25 @@ export default function StaticCard() {
     <main className="row static-container my-5 restau">
 
       <aside className="col-md-5 col-12">
-        <div>
+    
+        <div className='custom-img-container mx-3'>
           <img
-           src="/images/banner_3.jpg" 
-           alt="Restaurant" />
+           src="https://images.unsplash.com/photo-1636405189493-181ecf851006?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzk4fHxyZXN0YXVyYW50fGVufDB8fDB8fHww" 
+           alt="Restaurant" 
+           style={{objectFit:'cover'}}
+           />
         </div>
+        
+        
       </aside>
 
       <aside className="col-md-7 col-12">
         <div 
         ref={contentRef} 
         className='slide-content text-light'>
-          <h1 className='col-12 text-center'>Welcome to Our Restaurant Guide!</h1>
+          <h1 className='col-12 text-center welcome'>Welcome to Our Restaurant Guide!</h1>
           <p
-           className='col-12 text-center'>
+           className='col-12 text-center my-2'>
             Discover the best places to eat in town. Whether you're looking for cozy restaurants,
              a fine dining experience, or a casual eatery, we've got you covered.
              Check out some of our top recommendations</p>
