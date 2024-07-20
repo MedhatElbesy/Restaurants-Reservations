e9538886fcf16c5b2db2b13ef897a1ff51af458e
Blitz576 <anagy8050@gmail.com>
2024-07-20 18:23:05 +0300

    fix : send reservation mail

---
+++ Backend/resources/views/emails/success_payment.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Payment Success</title>
+</head>
+<body>
+<h1>Payment Successful</h1>
+<p>Dear {{ $userName }},</p>
+<p>We have received your payment of ${{ $paymentAmount }} on {{ $paymentDate }}.</p>
+<p>Thank you for your purchase!</p>
+</body>
+</html>
