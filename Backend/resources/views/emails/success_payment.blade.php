21fbf7ed3d7c5935ebdef2452c5fc5d61fda5d24
Blitz576 <anagy8050@gmail.com>
2024-07-20 18:27:58 +0300

    fix : content section name

--- Backend/resources/views/emails/success_payment.blade.php
+++ Backend/resources/views/emails/success_payment.blade.php
@@ -1,6 +1,6 @@
 @extends('emails.layout')
 @section('title', 'Successful Payment')
-@section('paymentSuccess')
+@section('content')
 <p>Dear {{ $userName }},</p>
 <p>We have received your payment of ${{ $paymentAmount }} on {{ $paymentDate }}.</p>
 <p>Thank you for your purchase!</p>
